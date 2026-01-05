d455c7e31e0f3709c3ac65e9627db0827c1af315
Henrik Fricke <henrikfricke@web.de>
2026-01-05 16:23:58 +0100

    feat: simple button

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,7 @@
 import { href, Link } from "react-router";
 import type { Route } from "./+types/home";
 import { hrefWithTrailingSlash } from "~/utils/href";
+import { Button } from "~/ui";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "pencil case" }];
@@ -8,11 +9,12 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return (
-    <>
+    <div className="inline-flex gap-4 flex-col">
       <h1>pencil case</h1>
       <Link to={hrefWithTrailingSlash("/legal-notice")}>
         Go to legal notice
       </Link>
-    </>
+      <Button>Button</Button>
+    </div>
   );
 }
