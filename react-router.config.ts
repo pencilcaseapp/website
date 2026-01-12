e0ae0a1b3b56754154a8c53cb8e2e92f1764080a
Henrik Fricke <henrikfricke@web.de>
2026-01-12 16:34:44 +0100

    fix: pr comments

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,5 +3,5 @@ import type { Config } from '@react-router/dev/config';
 export default {
   ssr: false,
   prerender: true,
-  basename: process.env.REACT_ROUTER_BASENAME || '/',
+  // basename: process.env.REACT_ROUTER_BASENAME || '/',
 } satisfies Config;
