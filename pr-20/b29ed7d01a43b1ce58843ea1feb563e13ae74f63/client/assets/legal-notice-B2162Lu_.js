2832d52c93df8d55fd65b6e8c0224ea70a545454
Henrik Fricke <henrikfricke@web.de>
2026-01-12 16:42:10 +0100

    refa: delete old folder structure

--- pr-20/b29ed7d01a43b1ce58843ea1feb563e13ae74f63/client/assets/legal-notice-B2162Lu_.js
+++
@@ -1,1 +0,0 @@
-import{w as t,l as e}from"./chunk-EPOLDU6W-eHO-Ryj-.js";function n(){return[{title:"Legal Notice"}]}const r=t(function(){return e.jsx("h1",{children:"Legal Notice"})});export{r as default,n as meta};
