a620b365d64e906873139e695d1208f40c3d1837
Henrik Fricke <henrikfricke@web.de>
2026-01-12 15:08:40 +0100

    fix: url for lost pixel

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -3,7 +3,7 @@ import type { CustomProjectConfig } from 'lost-pixel';
 export const config: CustomProjectConfig = {
   pageShots: {
     pages: [{ path: '/', name: 'home' }],
-    baseUrl: 'http://172.17.0.1:5173',
+    baseUrl: 'http://localhost:5173',
   },
   lostPixelProjectId: 'cmkb8aqi907x6c3jjrcm43luq',
   apiKey: process.env.LOST_PIXEL_API_KEY,
