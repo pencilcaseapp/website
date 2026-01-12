9ec7028aaa9e7cc4b059246f4a82283c1312c9bd
Henrik Fricke <henrikfricke@web.de>
2026-01-12 16:25:24 +0100

    refa: set react router basename for pr previews

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,4 +3,5 @@ import type { Config } from '@react-router/dev/config';
 export default {
   ssr: false,
   prerender: true,
+  basename: process.env.REACT_ROUTER_BASENAME || '/',
 } satisfies Config;
