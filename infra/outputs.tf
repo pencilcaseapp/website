e51c3dfed18f4021312c25b6865ed788408e2d68
Henrik Fricke <henrikfricke@web.de>
2026-01-26 22:14:51 +0100

    refa: clean up terraform

--- infra/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "domain" {
-  description = "The domain of the website"
-  value       = scaleway_object_bucket_website_configuration.website_bucket_website_configuration.website_domain
-}
-
-output "endpoint" {
-  description = "The endpoint URL of the website"
-  value       = scaleway_object_bucket_website_configuration.website_bucket_website_configuration.website_endpoint
-}
