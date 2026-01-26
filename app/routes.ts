3777227e9005b610c3125eb4da68f5865d2a36d5
Henrik Fricke <henrikfricke@web.de>
2026-01-26 23:37:29 +0100

    refa(vite): rename client assets folder (#41)

--- app/routes.ts
+++ app/routes.ts
@@ -2,5 +2,5 @@ import { type RouteConfig, index, route } from '@react-router/dev/routes';
 
 export default [
   index('routes/home.tsx'),
-  route('legal-notice', './routes/legal-notice.tsx'),
+  route('site/legal-notice', './routes/legal-notice.tsx'),
 ] satisfies RouteConfig;
