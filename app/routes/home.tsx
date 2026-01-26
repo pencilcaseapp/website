d8a22a00710fe5e5ce822bccf975e9a5389ed78c
Henrik Fricke <henrikfricke@web.de>
2026-01-26 23:35:53 +0100

    refa: put routes under /site

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="inline-flex gap-4 flex-col">
       <h1>pencil case</h1>
-      <Link to={href('/legal-notice')}>
+      <Link to={href('/site/legal-notice')}>
         Go to legal notice
       </Link>
       <Button>Button</Button>
